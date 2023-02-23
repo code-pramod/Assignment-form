22e4ef557d32fafb489ec63c6b08e05077d5c641
Pramod Shand <pramod_shand@yahoo.com>
2023-02-23 17:04:42 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Table from './Component/Table';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='row'>
+        <div className='col mx-2'>
+          <h1> My Timesheet</h1>
+        </div>
+      </div>
+      <br></br>
+
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <input type='date' className='form-control' />
+          </div>
+
+          <div className="col">
+            <div className='btn-group' role='group'>
+              <button className='btn btn-primary'>Day</button>
+              <button className='btn btn-secondary'>Week</button>
+            </div>
+          </div>
+
+          <div className="col-3">
+            <input type="checkbox" /> Show all assign timecodes
+          </div>
+          <div className="col-2">
+            <input placeholder='filter timecodes...' className='form-control' />
+          </div>
+          <div className="col mr-0">
+            <button className='btn btn-secondary'>Hide / Show / Gr..</button>
+          </div>
+        </div>
+      </div>
+      <br></br><br></br>
+
+      <div>
+        <Table />
+      </div >
+
+      <button className="btn btn-success mx-1"> Close</button>
+    </>
   );
 }
 
---
+++ src/Component/Table.js
@@ -0,0 +1,178 @@
+import React from 'react';
+
+const Table = () => {
+    return (
+        <>
+            <table align='center' width="100%">
+                <div className='container'>
+                    <div className='row'>
+                        <div className='col' >
+                            <form>
+                                <div className='row' >
+                                    <div className='col-4'>
+                                        <p align="left">Pin  Timecode</p>
+                                    </div>
+                                    <div className='col' align="center" >
+                                        <p >15</p>
+                                        <p>Mon</p>
+                                    </div>
+                                    <div className='col' align="center">
+                                        <p>16</p>
+                                        <p>Tue</p>
+                                    </div>
+                                    <div className='col' align="center">
+                                        <p>17</p>
+                                        <p>Wed</p>
+                                    </div>
+                                    <div className='col' align="center">
+                                        <p>18</p>
+                                        <p>Thu</p>
+                                    </div>
+                                    <div className='col' align="center">
+                                        <p>19</p>
+                                        <p>Fri</p>
+                                    </div>
+                                    <div className='col' align="center">
+                                        <p>20</p>
+                                        <p>Sat</p>
+                                    </div>
+                                    <div className='col'>
+                                        <div className='shadow-lg p-2 mb-0'>
+                                            <p>21</p>
+                                            <p>Sun</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <hr />
+                                <div className='row'>
+                                    <div className='col-4'>
+                                        <label> <b>EXTN NAB118 - Suhasini, Amit, Anu--Consulting </b></label>
+                                        <p>NAB</p>
+                                    </div>
+                                    <div className='col'>
+                                        <input className='form-control' />
+                                    </div>
+                                    <div className='col'>
+                                        <input className='form-control' />
+                                    </div>
+                                    <div className='col'>
+                                        <input className='form-control' />
+                                    </div>
+                                    <div className='col'>
+                                        <input className='form-control' />
+                                    </div>
+                                    <div className='col'>
+                                        <input className='form-control' />
+                                    </div>
+                                    <div className='col'>
+                                        <input className='form-control' />
+                                    </div>
+
+                                    <div className='col'>
+                                        <div className='shadow-lg p-3 mb-0'>
+                                            <input className='form-control' />
+                                        </div>
+                                    </div>
+                                </div>
+                                <hr />
+                                <div className='row'>
+                                    <div className='col-4'>
+                                        <p> <b>Internal (incl.migrated hours from Harvest)</b></p>
+                                        <p>Appscore</p>
+                                    </div>
+                                    <div className='col'>
+                                        <input className='form-control' />
+                                    </div>
+                                    <div className='col'>
+                                        <input className='form-control' />
+                                    </div>
+                                    <div className='col'>
+                                        <input className='form-control' />
+                                    </div>
+                                    <div className='col'>
+                                        <input className='form-control' />
+                                    </div>
+                                    <div className='col'>
+                                        <input className='form-control' />
+                                    </div>
+                                    <div className='col'>
+                                        <input className='form-control' />
+                                    </div>
+                                    <div className='col'>
+                                        <div className='shadow-lg p-3 mb-0'>
+                                            <input className='form-control' />
+                                        </div>
+                                    </div>
+                                </div>
+                                <hr />
+                                <div className='row'>
+                                    <div className='col-4'>
+                                        <label> <b>Internal (incl.migrated hours from Harvest)</b></label>
+                                        <p>Appscore</p>
+                                    </div>
+                                    <div className='col'>
+                                        <input className='form-control' />
+                                    </div>
+                                    <div className='col'>
+                                        <input className='form-control' />
+                                    </div>
+                                    <div className='col'>
+                                        <input className='form-control' />
+                                    </div>
+                                    <div className='col'>
+                                        <input className='form-control' />
+                                    </div>
+                                    <div className='col'>
+                                        <input className='form-control' />
+                                    </div>
+                                    <div className='col'>
+                                        <input className='form-control' />
+                                    </div>
+                                    <div className='col'>
+                                        <div className='shadow-lg p-3 mb-0'>
+                                            <input className='form-control' />
+                                        </div>
+                                    </div>
+                                    <hr />
+                                </div>
+                                <div className='row'>
+                                    <div className='col-4'>
+                                        <label> <b>Internal (incl.migrated hours from Harvest)</b></label>
+                                        <p>Appscore</p>
+                                    </div>
+                                    <div className='col'>
+                                        <input className='form-control' />
+                                    </div>
+                                    <div className='col'>
+                                        <input className='form-control' />
+                                    </div>
+                                    <div className='col'>
+                                        <input className='form-control' />
+                                    </div>
+                                    <div className='col'>
+                                        <input className='form-control' />
+                                    </div>
+                                    <div className='col'>
+                                        <input className='form-control' />
+                                    </div>
+                                    <div className='col'>
+                                        <input className='form-control' />
+                                    </div>
+                                    <div className='col'>
+                                        <div className='shadow-lg p-3 mb-0'>
+                                            <input className='form-control' />
+                                        </div>
+                                    </div>
+                                    <hr />
+                                </div>
+                                <button>+Add time entry</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </table >
+        </>
+    )
+}
+
+export default Table;
